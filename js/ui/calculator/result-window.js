696bcd11c144af3f75108be56be74dcc9cb0751e
Ilya Shishkin <mostbook@mail.ru>
2022-10-21 11:34:39 +0700

    Added autoresize for result window and customized scroll

--- js/ui/calculator/result-window.js
+++ js/ui/calculator/result-window.js
@@ -1,8 +1,17 @@
+const SYMBOLS_COUNT_TO_FONT_SIZE = {
+  '0': '96px',
+  '6': '80px',
+  '8': '64px',
+  '10': '48px',
+};
+
 export class ResultWindow {
   #element;
+  #fontSize;
 
   constructor( element ) {
     this.#element = element;
+    this.#fontSize = this.#element.style.fontSize;
   }
 
   set value( value ) {
@@ -11,11 +20,13 @@ export class ResultWindow {
 
   append( symbol ) {
     this.#element.textContent += symbol;
+    this.#resize();
   }
 
   pop() {
     const newText = this.#element.textContent.slice(0, -1);
     this.#element.textContent = newText;
+    this.#resize();
   }
 
   reset() {
@@ -24,5 +35,26 @@ export class ResultWindow {
 
   get length() {
     return this.#element.textContent.length;
+  }
+
+  #resize() {
+    const neededSize = this.#defineNeededSize();
+    if (neededSize === this.#fontSize) return;
+
+    this.#fontSize = neededSize;
+    this.#element.style.fontSize = neededSize;
+  }
+
+  #defineNeededSize() {
+    const symbolsCount = this.#element.textContent.length;
+
+    let fontSize;
+    Object.keys(SYMBOLS_COUNT_TO_FONT_SIZE).forEach(key => {
+      if (symbolsCount < key) return fontSize;
+
+      fontSize = SYMBOLS_COUNT_TO_FONT_SIZE[key];
+    });
+
+    return fontSize;
   }
 }
